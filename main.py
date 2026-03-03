bb67be0380416fe6ac5df6d79e828bd5bada3377
Juan Sánchez Casado <juansanchezcasado1@gmail.com>
2026-03-03 14:56:18 +0100

    terminado

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ while True:
     imprimirListado(dinoParque1)
     print()
     opciones()
-
     opcion = input("Selecciona opción: ")
-    
     if opcion == "O":
         print("Ordenar por:")
         print("1 - Nombre")
@@ -39,17 +37,17 @@ while True:
         peso = float(input("Peso: "))
         carnivoro = input("Es carnivoro [S/N]")
         if carnivoro == "S":
-            carnivoro == True
+            carnivoro = True
         else:
             False
         omnivoro = input("Es omnivoro [S/N]")
         if omnivoro == "S":
-            omnivoro == True
+            omnivoro = True
         else:
             False
         herbivoro = input("Es herbivoro [S/N]")
         if herbivoro == "S":
-            herbivoro == True
+            herbivoro = True
         else:
             False
         descubrimiento = input("Fecha de descubrimiento (dd-mm-yyyy): ")
@@ -59,6 +57,45 @@ while True:
         
         dinoAñadido = Dino(dinoId, nombre, habilidadesLista, altura, peso, carnivoro, omnivoro, herbivoro, descubrimientoFecha, esqueletoCompletoFecha )
         dinoParque1.añadirDino(dinoAñadido)
-
+    else:
+        idSeleccionado = int(opcion)
+        if idSeleccionado < 0:
+            eliminado = dinoParque1.eliminarDinobyID(abs(idSeleccionado))
+            if eliminado:
+                print("Dino eliminado correctamente.")
+            else:
+                print("No existe un dino con ese ID.")
+        elif idSeleccionado > 0:
+            dinoSeleccionado = None
+            for d in dinoParque1.DinosHabitantes:
+                if d.idDino == idSeleccionado:
+                    dinoSeleccionado = d
+                    break
+            if dinoSeleccionado is None:
+                print("No existe un dino con ese ID.")
+            else:
+                print("\nDino seleccionado:")
+                print(dinoSeleccionado)
+                while True:
+                    print("\n¿Qué quieres modificar?")
+                    print("1 - Nombre")
+                    print("2 - Altura")
+                    print("3 - Peso")
+                    print("4 - Habilidades")
+                    print("0 - Volver")
+                    campo = input("Selecciona campo: ")
+                    if campo == "1":
+                        dinoSeleccionado.nombre = input("Nuevo nombre: ")
+                    elif campo == "2":
+                        dinoSeleccionado.altura = float(input("Nueva altura: "))
+                    elif campo == "3":
+                        dinoSeleccionado.peso = float(input("Nuevo peso: "))
+                    elif campo == "4":
+                        nuevasHab = input("Habilidades separadas por comas: ")
+                        dinoSeleccionado.habilidades = nuevasHab.split(",")
+                    elif campo == "0":
+                        break
+                    else:
+                        print("Opción inválida.")
     if opcion == "S":
         break
